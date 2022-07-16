b113af0b1ee09000d29fa0c8a23697ae150951ed
sevenineone <alexeyka791@gmail.com>
2022-07-16 20:56:35 +0200

    some fixes, start to implement plot method.

--- backtesting/torgash.py
+++ backtesting/torgash.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import ta
+import plotly.graph_objects as go
 
 
 class Torgash:
@@ -12,13 +13,6 @@ class Torgash:
         self.min_bid_threshold = 0
         self.min_ask_threshold = 0
         ###################################
-        self.date_time = []
-        self.open = []
-        self.high = []
-        self.low = []
-        self.close = []
-        self.volume = []
-        ###################################
         self.transaction_type_hist = []
         self.transaction_amount_hist = []
         self.transaction_datetime = []
@@ -30,10 +24,6 @@ class Torgash:
         self.current_datetime = None
         self.current_price = 0
         ###################################
-
-    def set_base_symbol(self, symbol):
-        self.base_symbol = symbol
-
 
     def set_data(self, ohlcv_df: pd.DataFrame, date_time=0, open=1, high=2, low=3, close=4, volume=5):
         self.data = pd.DataFrame([
@@ -48,7 +38,12 @@ class Torgash:
             self.data['Datetime'] = pd.to_datetime(self.data['Datetime'], unit='ms')
         except:
             raise ValueError("Datetime column format must be unix or string format.")
-        self.data = ta.add_all_ta_features(self.data, 'Open', 'High', 'Low', 'Close', 'Volume', fillna=True)
+        # calculate indicators to dataframe
+        self.data = self.data.join(ta.trend.sma_indicator(self.data.Close, window=50))
+        self.data = self.data.join(ta.trend.sma_indicator(self.data.Close, window=100))
+        self.data.columns = ['Datetime', 'Open', 'High', 'Low', 'Close', 'Volume', 'sma_fast',
+                             'sma_slow']  # name the columns the way you want
+        #####################################################
         self.data.set_index('Datetime', inplace=True)
 
     def set_data_from_csv(self, filename: str, date_time=0, open=1, high=2, low=3, close=4, volume=5):
@@ -70,6 +65,7 @@ class Torgash:
                   f"{(amount / self.current_price) * self.trading_fee_multiplier} {self.trading_symbol} || "
                   f"Balance: {self.current_base_balance} {self.base_symbol} | "
                   f"{self.current_trading_balance} {self.trading_symbol} ||")
+            self.print_ind()  # xtra output for necessary indicators
         else:
             print("Buy attempt, but not enough money.")
 
@@ -88,25 +84,41 @@ class Torgash:
                   f"{(amount * self.current_price) * self.trading_fee_multiplier} {self.base_symbol} || "
                   f"Balance: {self.current_base_balance} {self.base_symbol} | "
                   f"{self.current_trading_balance} {self.trading_symbol} ||")
+            self.print_ind()  # xtra output for necessary indicators
         else:
             print("Sell attempt, but not enough money.")
 
+    def print_ind(self):  # put indicators u need (ex: )
+        print("")
 
     def run_strategy(self):
-        #####################################################
-        fast_above_slow = self.data.trend_sma_fast[self.data.head(1).index[0]] > self.data.trend_sma_slow[self.data.head(1).index[0]]
+        # We cut the top of the data, because first 100 sma values are not calculated there.
+        self.data = self.data.iloc[99:]
+        # xtra variables
+        fast_above_slow = self.data.sma_fast[self.data.head(1).index[0]] > self.data.sma_slow[
+            self.data.head(1).index[0]]
         #####################################################
         self.base_balance_hist.append(self.current_base_balance)
         self.trading_balance_hist.append(self.current_trading_balance)
         for datetime, value in self.data.iterrows():
             self.current_datetime = datetime
             self.current_price = value.Close
-            #####################################################
-            if fast_above_slow and self.data.trend_sma_fast[datetime] < self.data.trend_sma_slow[datetime]:
+            # There you should implement your strategy
+            if fast_above_slow and self.data.sma_fast[datetime] < self.data.sma_slow[datetime]:
                 self.sell()
-            elif not fast_above_slow and self.data.trend_sma_fast[datetime] > self.data.trend_sma_slow[datetime]:
+            elif not fast_above_slow and self.data.sma_fast[datetime] > self.data.sma_slow[datetime]:
                 self.buy()
-            fast_above_slow = self.data.trend_sma_fast[datetime] > self.data.trend_sma_slow[datetime]
+            fast_above_slow = self.data.sma_fast[datetime] > self.data.sma_slow[datetime]
             #####################################################
             self.base_balance_hist.append(self.current_base_balance)
             self.trading_balance_hist.append(self.current_trading_balance)
+
+    def plot(self):
+        fig = go.Figure(data=[go.Candlestick(x=self.data.index.values,
+                                             open=self.data['Open'], high=self.data['High'],
+                                             low=self.data['Low'], close=self.data['Close'])
+                              ])
+
+        fig.update_layout(xaxis_rangeslider_visible=False)
+        fig.show()
+        # coming soon...
