dd1062239182f36ef526ab48c6e54d9748290732
KasperZ <40818603+zaytseffffffff@users.noreply.github.com>
2022-07-27 15:09:52 +0300

    Add limit and market order (not tested)

--- backtesting/torgash.py
+++ backtesting/torgash.py
@@ -2,28 +2,71 @@ import pandas as pd
 import ta
 import ccxt
 import plotly.graph_objects as go
+from typing import List
+
+
+class Order:
+    """
+    ccxt has more class fields
+    """
+    def __init__(self, id, datetime, symbol, type, side, average, amount, info=""):
+        self.id = id
+        self.datetime = datetime
+        self.datetime_execute = datetime  # CCXT Order DON'T HAVE THIS VARIABLE
+        self.symbol = symbol
+        self.type = type  # 'limit', 'market'
+        self.side = side  # 'buy', 'sell'
+        self.average = average  # price
+        self.amount = amount
+        self.info = info
+
+    def set_datetime_execute(self, datetime_execute):
+        self.datetime_execute = datetime_execute
+
+
+class Trades:
+    def __init__(self):
+        self.open_order: List[Order, ] = []   # [Order, ] orders are open but not executed
+        self.open_trade: List[Order, ] = []  # [Order, ] open position
+        self.close_trade: List[List[Order, ]] = []  # [(Order, Order), ] two opposite order
+
+    def open_trade_amount(self):
+        amount = 0
+        for trade in self.open_trade:
+            if trade.side == 'buy':
+                amount += trade.amount
+            else:
+                amount -= trade.amount
+        return abs(amount)
+
+    def open_trade_side(self):
+        if len(self.open_trade) > 0:
+            return self.open_trade[0].side
+        return None
 
 
 class Torgash:
     def __init__(self):
-        self.current_base_balance = 90000000
+        self.current_base_balance = 1000
         self.base_symbol = 'usdt'
         self.current_trading_balance = 0
         self.trading_symbol = 'btc'
+        self.symbol = "BTCUSDT"
         self.trading_fee_multiplier = 1 - 0.0002
         self.min_order_threshold = 5  # equal 5usdt for binance futures
         self.min_order_step = 0.001   # min step for trading_symbol (btc) 0.001btc binance futures
         ###################################
-        self.transaction_type_hist = []
-        self.transaction_amount_hist = []
-        self.transaction_datetime = []
+        self.transaction_type_hist = []    # надо убрать
+        self.transaction_amount_hist = []  # надо убрать
+        self.transaction_datetime = []     # надо убрать
+        self.trades = Trades()
         ###################################
         self.base_balance_hist = []
         self.trading_balance_hist = []
         ###################################
         self.data = None
-        self.current_datetime = None
-        self.current_price = 0
+        self._current_datetime = None
+        self._current_price = 0
         ###################################
 
     def set_data(self, ohlcv_df: pd.DataFrame, date_time=0, open=1, high=2, low=3, close=4, volume=5):
@@ -50,7 +93,39 @@ class Torgash:
         ohlcv_df = pd.read_csv(filename)
         self.set_data(ohlcv_df, date_time=date_time, open=open, high=high, low=low, close=close, volume=volume)
 
-    def createOrder(self, symbol, type, side, amount, price = None, params = {}):
+    def execute_order(self, order: Order):
+        if not self.trades.open_trade:  # if there are no open trade
+            self.trades.open_trade.append(order)
+        elif self.trades.open_trade_side() == order.side:  # if the direction is the same
+            self.trades.open_trade.append(order)
+        else:  # orders are sent in different directions
+            if self.trades.open_trade_amount() == order.amount:
+                self.trades.open_trade.append(order)
+                self.trades.close_trade.append(self.trades.open_trade)
+                self.trades.open_trade = []
+            elif self.trades.open_trade_amount() > order.amount:
+                self.trades.open_trade.append(order)
+            else:  # open trade amount less than new order amount
+                raise ValueError("Incorrect order amount")
+
+    def limits_check(self):
+        for i in len(self.trades.open_order):
+            if self.trades.open_order[i].side == 'buy':
+                # if buy but limit price > self._current_price (make market order) !!!!!
+
+                if self.data[self._current_datetime].Low < self.trades.open_order[i].average:
+                    order = self.trades.open_order.pop(i)
+                    order.set_datetime_execute(self._current_datetime)
+                    self.execute_order(order)
+            elif self.trades.open_order[i].side == 'sell':
+                # if sell but limit price < self._current_price (make market order) !!!!!
+
+                if self.data[self._current_datetime].High > self.trades.open_order[i].average:
+                    order = self.trades.open_order.pop(i)
+                    order.set_datetime_execute(self._current_datetime)
+                    self.execute_order(order)
+
+    def createOrder(self, symbol, type, side, amount, price=None, params={}):
         """
         :param symbol: (String) required Unified CCXT market symbol
         :param type: "market" "limit" see #custom-order-params and #other-order-types for non-unified types
@@ -66,42 +141,104 @@ class Torgash:
         ex:
         mk.create_order(symbol='XRP/BTC', type='stop_loss_limit', side='sell', amount=1,
                         price=0.6, params={'stopPrice':0.60})
+        """
 
+        order = Order(0, self._current_datetime, symbol, type, side, average=price, amount=amount)
+        if type == "limit":
+            self.trades.open_order.append(order)
+        elif type == "market":
+            self.execute_order(order)
+        else:
+            raise ValueError("Bad type of transaction")
+
+    def createLimitBuyOrder (self, symbol, amount, price, params={}):
+        """
+        How in ccxt
+
+        :return:
+        """
+        return self.createOrder(symbol=symbol, type="limit", side="buy", amount=amount, price=price, params=params)
+
+    def createLimitSellOrder (self, symbol, amount, price, params={}):
+        """
+        How in ccxt
+
+        :return:
+        """
+        return self.createOrder(symbol=symbol, type="limit", side="sell", amount=amount, price=price, params=params)
+
+    def createMarketOrder (self, symbol, side, amount, params):
+        return self.createOrder(symbol=symbol, type="market", side=side, amount=amount, params=params)
+
+    def fetchOpenOrders(self, ): # вывод id всех открытых ордеров
+        """
+        How in ccxt
+
+        :return:
         """
         pass
 
-    def buy(self, amount=-1., ):
+    def fetchClosedOrders(self):
+        """
+        How in ccxt
+
+        :return:
+        """
+        pass
+
+    def fetchPosition(self, symbol, params={}):
+        """
+
+        :param symbol: (String) required Unified CCXT market symbol ex:"BTC|USDT"
+        :param params:
+        :return:
+        """
+        pass
+
+    def cancelOrder(self, id, symbol, params = {}):
+        """
+        How in ccxt
+
+        :param id:
+        :param symbol:
+        :param params:
+        :return:
+        """
+        pass
+
+
+    def buy(self, amount=-1., ):  # надо убрать
         if amount < 0:
             amount = self.current_base_balance
         if self.current_base_balance >= amount >= self.min_order_threshold:
             self.current_base_balance -= amount
-            self.current_trading_balance += (amount / self.current_price) * self.trading_fee_multiplier
+            self.current_trading_balance += (amount / self._current_price) * self.trading_fee_multiplier
             ###
-            self.transaction_datetime.append(self.current_datetime)
+            self.transaction_datetime.append(self._current_datetime)
             self.transaction_type_hist.append('buy')
             self.transaction_amount_hist.append(amount)
             ###
-            print(f"|| {self.current_datetime} | Buy | {amount} {self.base_symbol} --> "
-                  f"{(amount / self.current_price) * self.trading_fee_multiplier} {self.trading_symbol} || "
+            print(f"|| {self._current_datetime} | Buy | {amount} {self.base_symbol} --> "
+                  f"{(amount / self._current_price) * self.trading_fee_multiplier} {self.trading_symbol} || "
                   f"Balance: {self.current_base_balance} {self.base_symbol} | "
                   f"{self.current_trading_balance} {self.trading_symbol} ||")
             self.print_ind()  # xtra output for necessary indicators
         else:
             print("Buy attempt, but not enough money.")
 
-    def sell(self, amount=-1., ):
+    def sell(self, amount=-1., ):  # надо убрать
         if amount < 0:
             amount = self.current_trading_balance
         if self.current_trading_balance >= amount >= self.min_order_threshold:
             self.current_trading_balance -= amount
-            self.current_base_balance += (amount * self.current_price) * self.trading_fee_multiplier
+            self.current_base_balance += (amount * self._current_price) * self.trading_fee_multiplier
             ###
-            self.transaction_datetime.append(self.current_datetime)
+            self.transaction_datetime.append(self._current_datetime)
             self.transaction_type_hist.append('buy')
             self.transaction_amount_hist.append(amount)
             ###
-            print(f"|| {self.current_datetime} | Sell | {amount} {self.trading_symbol} --> "
-                  f"{(amount * self.current_price) * self.trading_fee_multiplier} {self.base_symbol} || "
+            print(f"|| {self._current_datetime} | Sell | {amount} {self.trading_symbol} --> "
+                  f"{(amount * self._current_price) * self.trading_fee_multiplier} {self.base_symbol} || "
                   f"Balance: {self.current_base_balance} {self.base_symbol} | "
                   f"{self.current_trading_balance} {self.trading_symbol} ||")
             self.print_ind()  # xtra output for necessary indicators
@@ -110,6 +247,20 @@ class Torgash:
 
     def print_ind(self):  # put indicators u need (ex: )
         print("")
+
+    def step_strategy(self, datetime):
+        pass
+
+    def run_step_strategy(self):
+        self.base_balance_hist.append(self.current_base_balance)
+        for datetime, value in self.data.iterrows():
+            self._current_datetime = datetime
+            self._current_price = self.data[datetime].Open
+            self.step_strategy(datetime)
+            self.limits_check()
+
+            #  self.base_balance_hist.append(self.current_base_balance)
+            #  self.trading_balance_hist.append(self.current_trading_balance)
 
     def run_strategy(self):
         # We cut the top of the data, because first 100 sma values are not calculated there.
@@ -121,8 +272,8 @@ class Torgash:
         self.base_balance_hist.append(self.current_base_balance)
         self.trading_balance_hist.append(self.current_trading_balance)
         for datetime, value in self.data.iterrows():
-            self.current_datetime = datetime
-            self.current_price = value.Close
+            self._current_datetime = datetime
+            self._current_price = value.Close
             # There you should implement your strategy
             if fast_above_slow and self.data.sma_fast[datetime] < self.data.sma_slow[datetime]:
                 self.sell()
